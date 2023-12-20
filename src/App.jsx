88322440ec21cc0f825050922a5048a79edf6f57
AndyGrld <ansongandy04@gmail.com>
2023-12-20 22:25:13 +0000

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import MyForm from './form'
 import './index.css'
 import MyForm2 from './form2'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MyForm3 from './form3'
 
 function App() {
 
   return (
-    <>
-    {/* <MyForm /> */}
-      {/* <MyForm2 /> */}
-      <MyForm3/>
-      </>
+    <Router>
+      <Routes>
+        <Route exact path="/Form1" element={<MyForm />}></Route>
+        <Route exact path="/Form2" element={<MyForm2 />}></Route>
+        <Route exact path="/" element={<MyForm3/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
